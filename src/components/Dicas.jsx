01b3f939a117ab295b1c9357801cd696a96d1675
Daniel Silva <dansilvac254@gmail.com>
2024-12-23 09:44:02 -0300

    utilizando o tailwind melhor

--- src/components/Dicas.jsx
+++ src/components/Dicas.jsx
@@ -3,11 +3,11 @@ import { useState } from "react";
 
 export default function Dicas({ dicas, onClickLike }) {
   return (
-    <div className="w-full h-auto grid-cols-2  max-[700px]:grid-cols-none max-[700px]:flex-col max-[700px]:justify-center grid">
+    <div className="w-full h-auto gap-5  max-[700px]:grid-cols-none grid-cols-2 grid">
       {dicas.map((dica) => (
         <section
           key={dica.id}
-          className="bg-[#D5CFE1] w-[280px] h-[280px] rounded-[50px] m-5 flex flex-col justify-between"
+          className="bg-[#D5CFE1] w-full rounded-[50px] flex flex-col justify-between"
         >
           <div className="w-[100%] h-[80px] rounded-t-[50px] flex justify-center items-center bg-[#464655] text-[20px] text-white p-5 font-bold ">
             <h1>{dica.title}</h1>
